01d61b77d09d7219d3a4e325d30513a22c834548
richard <randria@users.noreply.github.com>
2022-03-28 10:47:08 +0200

    Update finish_install.sh

--- vm-on-liccia/finish_install.sh
+++ vm-on-liccia/finish_install.sh
@@ -4,14 +4,15 @@
 MASTERIP=${MASTERIP:-172.20.52.109}
 MOUNTPOINT=${MOUNTPOINT:-/opt/data}
 JUPYHOME=${MOUNTPOINT}/jupyterlab
-UBUNHOME=${MOUNTPOINT}/ubuntu_home
-HOMEU=${HOMEU:-/home/$USER}
+CONFUSER=${CONFUSER:-$USER}
+UBUNHOME=${MOUNTPOINT}/${CONFUSER}_home
+HOMEU=${HOMEU:-/home/$CONFUSER}
 #
 # Variables must not be changed
 CURRENTIP=$(ip a | grep inet.172 | awk '{print $2}' | awk -F'/' '{print $1}')
 #
 #
-if [ "$MASTERIP" != "$CURRENTIP" ]; then
+if [ "$MASTERIP" = "$CURRENTIP" ]; then
    echo "** nothing will be done on the master **"
    exit 1
 fi
@@ -25,29 +26,53 @@ init(){
    mkdir -p $MOUNTPOINT 2>/dev/null && echo ..ok || echo "**nok (exists already or issue)"
 }
 
-jupyter(){
-mkdir -p $JUPYHOME $UBUNHOME 2>/dev/null
+conf_jupyter(){
 if [ -d $MOUNTPOINT ]; then
+   mkdir -p $JUPYHOME $UBUNHOME 2>/dev/null
    # configuring jupyter user home directory
    chown root:jupyterhub-users $JUPYHOME
    chmod 1777 $JUPYHOME
 
-   # move .local
-   chown ubuntu:ubuntu $UBUNHOME
-   chmod 0754 $UBUNHOME
-   if [ ! -h $HOMEU/.local ]; then
-      if [ ! -d $UBUNHOME/.local ]; then  
-         ( cd $HOMEU && mv -v $HOMEU/.local $UBUNHOME/ )
-      fi
-      ( cd $HOMEU && ln -fs $UBUNHOME/.local )
-   fi
+   # move dirs on jupteradmin
+   for user in jupyter-jupyadmin ubuntu; do
+     echo "-- Moving dirs for [$user]"
+     case $user in
+        "jupyter-jupyadmin")
+        export OPTHOME="$JUPYHOME/$user"
+        ;;
+        "ubuntu")
+        export OPTHOME=$UBUNHOME
+        ;;
+     esac
+     mkdir -p $OPTHOME 2>/dev/null
+     chown $user:$user ${OPTHOME}
+     ( cd /home/$user ;
+       for dir in .local .cache .conda; do
+         mkdir -p ${OPTHOME}/$dir;
+	 if [ ! -h $dir -a -d $dir ]; then
+            rm -rf ${OPTHOME}/$dir
+            mv -fv $dir $OPTHOME/
+         fi
+         ln -fs ${OPTHOME}/$dir;
+       done
+     ) 2>/dev/null
+   done
+
+   # update jupyterlab workdir
+   rsync -auq /opt/jupyterlab_workdir/ $JUPYHOME/ 2>/dev/null
 
    # tljh optimization
+   # nativeauthenticator.NativeAuthenticator
+   JUPYCONFIG=/opt/tljh/config/jupyterhub_config.d/jupyterhub_config.py
+   if [[ ! `grep ^c.Spawner.notebook_dir $JUPYCONFIG 2>/dev/null ]]; then
+      echo "c.Spawner.notebook_dir='$JUPYHOME'" >> $JUPYCONFIG
+   fi
    tljh-config set limits.memory $(head -1 /proc/meminfo | awk '{print $2*0.85"K"}')
    tljh-config set limits.cpu $(echo "`nproc` * 0.8" | bc | awk '{print int($0) }')
    tljh-config set user_environment.default_app jupyterlab
-   tljh-config set auth.type nativeauthenticator.NativeAuthenticator
+   tljh-config unset auth.type 
    tljh-config reload
+   tljh-config show
 else
    init
 fi
@@ -56,26 +81,30 @@ fi
 #
 # input $1 : device path ex. /dev/sdb
 #
-format(){
-  FINDDEV=$(dmesg | grep -i sdb: | awk -F': ' '{print "/dev/"$2}' | awk '{print $1}')
+do_format(){
+  FINDDEV=$(dmesg | grep -i sdb: | awk -F': ' '{print "/dev/"$2}' | awk '{print $1}' | tr -d '[0-9]')
   INDEV=${1:-$FINDDEV}
   DEV=${INDEV:-/dev/sdb}
+  echo -n "** Read to destroy data in [$DEV] (y/N)? "; read ANS
+  [ "$ANS" != "y" ] && return 1
+  umount $MOUNTPOINT 2>/dev/null
   parted $DEV mklabel gpt && parted $DEV mkpart primary ext4 0% 100% && {
   parted $DEV print && mkfs.ext4 -F ${DEV}1 || exit 1
   mkdir -p $MOUNTPOINT 2>/dev/null
-  if [ -z "`grep $DEV /etc/fstab 2>/dev/null`" ]; then
+  if [ -z "`grep ^$DEV /etc/fstab 2>/dev/null`" ]; then
      echo '${DEV} $MOUNTPOINT ext4 defaults 0 0' >> /etc/fstab
   fi
   }
   mount -a
 }
 
-docker(){
-if [ -d $MOUNTPOINT ]; then
+conf_docker(){
+if [ -d $MOUNTPOINT ]; then 
+   mkdir -p $MOUNTPOINT/docker 2>/dev/null
    if [ ! -h /var/lib/docker -a -d /var/lib/docker ]; then
       if [ -d $MOUNTPOINT/docker ]; then
 	 rsync -auq --delete /var/lib/docker/ $MOUNTPOINT/docker/ 2>/dev/null
-	 rm -f /var/lib/docker 2>/dev/null
+	 rm -rf /var/lib/docker 2>/dev/null
       else
 	 mv -f /var/lib/docker $MOUNTPOINT/ 2>/dev/null
       fi
@@ -90,13 +119,14 @@ fi
 
 #
 # init
-# format [/dev/sdb]
-# jupyter
-# docker
+# do_format [/dev/sdb]
+# conf_jupyter
+# conf_docker
 #
 cat<<EOF
 --------------------------------------------
 -- Editable variables ----------------------
+CONFUSER=$CONFUSER
 MASTERIP=$MASTERIP
 MOUNTPOINT=$MOUNTPOINT
 JUPYHOME=$JUPYHOME
@@ -104,7 +134,5 @@ UBUNHOME=$UBUNHOME
 HOMEU=$HOMEU
 --------------------------------------------
 EOF
-for i in "$@"; do
-   echo $i
-done
+$@
 exit 0
